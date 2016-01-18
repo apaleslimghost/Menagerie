f2f011828982659626c6a4f1b9af3354d7f80be4
Matt Brennan <mattyb1000@gmail.com>
2016-01-18 15:12:34 +0000

    title

--- components/spellcard.js
+++ components/spellcard.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-var SpellCard = ({spell}) => <article>
-	<h1>{spell.name}</h1>
-	<a href="/">back</a>
-</article>;
+class SpellCard extends React.Component {
+	title() {
+		return this.props.spell.name;
+	}
+
+	render() {
+		return <article>
+			<h1>{this.props.spell.name}</h1>
+			<a href="/">back</a>
+		</article>;
+	}
+}
 
 export default SpellCard;
