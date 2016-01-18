c978fdb80fba826daa79612a38751564c32a7a4d
Matt Brennan <mattyb1000@gmail.com>
2016-01-18 15:12:45 +0000

    modules

--- db/server.js
+++ db/server.js
@@ -2,26 +2,17 @@ import PouchDB from 'pouchdb';
 import memdown from 'memdown';
 import express from 'express';
 import morgan  from 'morgan';
-import expressPouchDB from 'express-pouchdb'
+import expressPouchDB from 'express-pouchdb';
+import wideOpen from '@quarterto/express-cors-wide-open';
+import options204 from '@quarterto/options-204';
 
 var MemPouch = PouchDB.defaults({db: memdown});
 
 var app = express();
 app.use(morgan('dev'));
-app.use(function(req, res, next) {
-	res.set('access-control-allow-origin', req.headers.origin);
-	res.set('access-control-allow-credentials', true);
-	res.set('access-control-allow-headers', req.headers['access-control-request-headers']);
-	res.set('access-control-allow-method', '*');
-	next();
-});
-app.use(function(req, res, next) {
-	if(req.method !== 'OPTIONS') return next();
-	res.sendStatus(204);
-})
+app.use(wideOpen);
+app.use(options204);;
 app.use(expressPouchDB(MemPouch));
-
-var port = 3001;
 app.listen(3001, console.log.bind(console, 'database listening on 3001'));
 
 export default MemPouch;
