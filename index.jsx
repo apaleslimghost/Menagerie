432840e876dd85fe311b14fb4522bee16a70e9be
Matt Brennan <mattyb1000@gmail.com>
2016-02-22 23:07:06 +0000

    do a redux

--- index.jsx
+++ index.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {Provider} from 'react-redux'
+import {syncHistory, routeReducer} from 'react-router-redux';
+
+import * as reducers from './reducers';
 
 import SpellCard from './components/spellcard.jsx';
 import List from './components/list.jsx';
 
 import spells from './spells';
 
+const reducer = combineReducers({
+	...reducers,
+	routing: routeReducer
+});
+
+const reduxRouterMiddleware = syncHistory(browserHistory);
+const store = createStore(
+	reducer,
+	{},
+	applyMiddleware(reduxRouterMiddleware)
+);
+
 const withProps = (Component, moreProps) => props => <Component {...moreProps} {...props} />;
 
-ReactDOM.render(<Router history={browserHistory}>
-	<Route path="/all-spells" component={withProps(List, {spells})}/>
-</Router>, document.querySelector('main'));
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={browserHistory}>
+			<Route path="/all-spells" component={withProps(List, {spells})}/>
+		</Router>
+	</Provider>,
+	document.querySelector('main')
+);
---
+++ reducers/index.js
@@ -0,0 +1,1 @@
+export spellsFilter from './filter';
