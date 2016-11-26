41935f2d7f1b53da690ca7a7d48b46a7dbed5c6b
Matt Brennan <mattyb1000@gmail.com>
2016-11-26 15:14:50 +0000

    correctly render cantrip

--- components/spellcard.jsx
+++ components/spellcard.jsx
@@ -6,7 +6,12 @@ class SpellCard extends React.Component {
 	render() {
 		return <article>
 			<h1>{this.props.spell.name}</h1>
-			<p>{this.props.spell.level}th-level {this.props.spell.school}</p>
+			<p>
+				{this.props.spell.level > 0 ?
+					`${this.props.spell.level}th-level ${this.props.spell.school}` :
+					`${this.props.spell.school} cantrip`
+				}
+			</p>
 			<Markdown text={this.props.spell['original-description']} />
 			<Link to="/all-spells">back</Link>
 		</article>;
